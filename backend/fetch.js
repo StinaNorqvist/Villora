22c76598c97e05ebb0773c953b9d8827af235023
StinaNorqvist <norqvist.stina@gmail.com>
2023-05-08 11:50:37 +0200

    First fetch on house. Mobcoded.

--- backend/fetch.js
+++ backend/fetch.js
@@ -1,10 +1,11 @@
 console.log("hello world!");
 
 function house() {
-  fetch("http://localhost:3000/api/house")
+  return fetch("http://localhost:3000/api/house")
     .then((response) => response.json())
     .then(function (data) {
       console.log(data, "testar");
+      return data;
     });
 }
 
---
+++ frontend/src/components/HousePlans.vue
@@ -0,0 +1,25 @@
+<script>
+  import fetch from '../../../backend/fetch.js'
+
+  export default {
+    name: 'HousePlans',
+    data() {
+      return {
+        houseItems: null
+      }
+    },
+
+    async created() {
+      this.houseItems = await fetch.house()
+      console.log(this.houseItems, 'HEJHEJEHEJEJE')
+    }
+  }
+</script>
+
+<template>
+  <div class="houseList" v-if="houseItems">
+    <div class="houseItem" v-for="houseItem in houseItems" :key="houseItem.Id">
+      <p>{{ houseItem }}</p>
+    </div>
+  </div>
+</template>
