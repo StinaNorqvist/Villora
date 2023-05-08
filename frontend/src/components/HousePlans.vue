b12da2f39f797a64f08002c02b42002850eed091
abdul <abdulhussee88@gmail.com>
2023-05-08 20:38:31 +0200

    Filteringsystem for floors to there child values

--- frontend/src/components/HousePlans.vue
+++ frontend/src/components/HousePlans.vue
@@ -26,79 +26,105 @@
     },
 
     computed: {
-      houseItemsSorted() {
-        if (!this.houseItems || !this.sortOption) return null
+      houseItemsProcessed() {
+        if (!this.houseItems) return null
 
-        const sortedItems = [...this.houseItems]
-        if (this.sortOption === 'name') {
-          sortedItems.sort((a, z) => a.houseName.localeCompare(z.houseName))
-        } else if (this.sortOption === 'price') {
-          sortedItems.sort((low, high) => low.housePrice - high.housePrice)
-        } else if (this.sortOption === 'floors') {
-          sortedItems.sort((low, high) => low.houseFloors - high.houseFloors)
-        } else if (this.sortOption === 'bedrooms') {
-          sortedItems.sort(
-            (low, high) => low.houseBedrooms - high.houseBedrooms
-          )
-        } else if (this.sortOption === 'bathrooms') {
-          sortedItems.sort(
-            (low, high) => low.houseBathrooms - high.houseBathrooms
-          )
+        let processedItems = [...this.houseItems]
+
+        // Filtering
+        if (this.filterOption) {
+          if (this.filterOption === '1floor') {
+            processedItems = processedItems.filter((item) => item.houseFloors === 1)
+          } else if (this.filterOption === '2floor') {
+            processedItems = processedItems.filter((item) => item.houseFloors === 2)
+          } else if (this.filterOption === '3floor') {
+            processedItems = processedItems.filter((item) => item.houseFloors === 3)
+          }
         }
 
-        return sortedItems
-      },
-
-      houseItemsFiltered() {
-        if (!this.houseItems || !this.filterOption) return null
-
-        let filterItems = [...this.houseItems]
-
-        if (this.filterOption === '1floor') {
-          filterItems = filterItems.filter((item) => item.houseFloors === 1)
-        } else if (this.filterOption === '2floor') {
-          filterItems = filterItems.filter((item) => item.houseFloors === 2)
-        } else if (this.filterOption === '3floor') {
-          filterItems = filterItems.filter((item) => item.houseFloors === 3)
+        // Sorting
+        if (this.sortOption) {
+          if (this.sortOption === 'name') {
+            processedItems.sort((a, z) => a.houseName.localeCompare(z.houseName))
+          } else if (this.sortOption === 'price') {
+            processedItems.sort((low, high) => low.housePrice - high.housePrice)
+          } else if (this.sortOption === 'floors') {
+            processedItems.sort((low, high) => low.houseFloors - high.houseFloors)
+          } else if (this.sortOption === 'bedrooms') {
+            processedItems.sort((low, high) => low.houseBedrooms - high.houseBedrooms)
+          } else if (this.sortOption === 'bathrooms') {
+            processedItems.sort((low, high) => low.houseBathrooms - high.houseBathrooms)
+          }
         }
 
-        return filterItems
+        return processedItems
       }
     }
   }
 </script>
 
+
 <template>
   <!-- FILTER BUTTONS -->
-  <button @click="handleFilterOption('1floor')">1 Floor</button>
-  <button @click="handleFilterOption('2floor')">2 Floor</button>
-  <button @click="handleFilterOption('3floor')">3 Floor</button>
-
-  <!-- SORT BUTTONS -->
-  <button @click="handleSortOption('name')">Sort by Name</button>
-  <button @click="handleSortOption('price')">Sort by Price</button>
-  <button @click="handleSortOption('floors')">Sort by Floors</button>
-  <button @click="handleSortOption('bedrooms')">Sort by Bedrooms</button>
-  <button @click="handleSortOption('bathrooms')">Sort by Bathrooms</button>
-
-  <div class="houseList" v-if="houseItemsSorted">
-    <div
-      class="houseItem"
-      v-for="houseItem in houseItemsSorted"
-      :key="houseItem.Id"
+  <div class="filter-buttons">
+    <button
+      @click="handleFilterOption('1floor')"
+      :class="{ active: filterOption === '1floor' }"
     >
-      <p>Name: {{ houseItem.houseName }}</p>
-      <p>Price: {{ houseItem.housePrice }} $</p>
-      <p>Floors: {{ houseItem.houseFloors }}</p>
-      <p>Bedrooms: {{ houseItem.houseBedrooms }}</p>
-      <p>Bathrooms: {{ houseItem.houseBathrooms }}</p>
-    </div>
+      1 Floor
+    </button>
+    <button
+      @click="handleFilterOption('2floor')"
+      :class="{ active: filterOption === '2floor' }"
+    >
+      2 Floor
+    </button>
+    <button
+      @click="handleFilterOption('3floor')"
+      :class="{ active: filterOption === '3floor' }"
+    >
+      3 Floor
+    </button>
   </div>
 
-  <div class="houseList" v-if="houseItemsFiltered">
+  <!-- SORT BUTTONS -->
+  <div class="sort-buttons">
+    <button
+      @click="handleSortOption('name')"
+      :class="{ active: sortOption === 'name' }"
+    >
+      Sort by Name
+    </button>
+    <button
+      @click="handleSortOption('price')"
+      :class="{ active: sortOption === 'price' }"
+    >
+      Sort by Price
+    </button>
+    <button
+      @click="handleSortOption('floors')"
+      :class="{ active: sortOption === 'floors' }"
+    >
+      Sort by Floors
+    </button>
+    <button
+      @click="handleSortOption('bedrooms')"
+      :class="{ active: sortOption === 'bedrooms' }"
+    >
+      Sort by Bedrooms
+    </button>
+    <button
+      @click="handleSortOption('bathrooms')"
+      :class="{ active: sortOption === 'bathrooms' }"
+    >
+      Sort by Bathrooms
+    </button>
+  </div>
+
+  <div class="houseList" v-show="houseItemsProcessed">
     <div
       class="houseItem"
-      v-for="houseItem in houseItemsFiltered"
+      v-for="houseItem in houseItemsProcessed"
       :key="houseItem.Id"
     >
       <p>Name: {{ houseItem.houseName }}</p>
@@ -109,3 +135,53 @@
     </div>
   </div>
 </template>
+
+<style scoped>
+.filter-buttons {
+  display: flex;
+  margin-bottom: 10px;
+}
+
+.sort-buttons {
+  display: flex;
+  margin-bottom: 20px;
+}
+
+.filter-buttons button,
+.sort-buttons button {
+  margin-right: 10px;
+  padding: 8px 12px;
+  font-size: 14px;
+  border: 1px solid #ccc;
+  background-color: transparent;
+  cursor: pointer;
+}
+
+.filter-buttons button {
+  background-color: #2c3e50;
+  color: #fff;
+}
+
+.sort-buttons button {
+  background-color: #34495e;
+  color: #fff;
+}
+
+.filter-buttons button.active,
+.sort-buttons button.active {
+  background-color: #e74c3c;
+}
+
+.houseList {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+}
+
+.houseItem {
+  padding: 15px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  background-color: #f7f7f7;
+}
+</style>
