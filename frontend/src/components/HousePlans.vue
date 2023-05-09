d3b00b5c119de492ef92fc1c72c24beedf316f15
abdul <abdulhussee88@gmail.com>
2023-05-09 14:03:35 +0200

    modified filteringsystem so users can select/ unselect and multiselect.

--- frontend/src/components/HousePlans.vue
+++ frontend/src/components/HousePlans.vue
@@ -6,8 +6,9 @@
     data() {
       return {
         houseItems: null,
-        sortOption: null,
-        filterOption: null
+        sortOptions: [],
+        filterOptions: [],
+        priceSortOrder: 'lowToHigh'
       }
     },
 
@@ -18,10 +19,31 @@
 
     methods: {
       handleSortOption(option) {
-        this.sortOption = option
+        if (this.sortOptions.includes(option)) {
+          this.sortOptions = this.sortOptions.filter(o => o !== option);
+          this.priceSortOrder = 'lowToHigh';
+        } else {
+          this.sortOptions.push(option);
+          if (option === 'price') {
+            this.priceSortOrder = 'lowToHigh';
+          }
+        }
       },
       handleFilterOption(option) {
-        this.filterOption = option
+        if (this.filterOptions.includes(option)) {
+          this.filterOptions = this.filterOptions.filter(o => o !== option);
+        } else {
+          this.filterOptions.push(option);
+        }
+      },
+      handlePriceSortOrder() {
+        if (this.priceSortOrder === 'lowToHigh') {
+          this.houseItemsProcessed.sort((low, high) => high.housePrice - low.housePrice);
+          this.priceSortOrder = 'highToLow';
+        } else {
+          this.houseItemsProcessed.sort((low, high) => low.housePrice - high.housePrice);
+          this.priceSortOrder = 'lowToHigh';
+        }
       }
     },
 
@@ -32,32 +54,30 @@
         let processedItems = [...this.houseItems]
 
         // Filtering
-        if (this.filterOption) {
-          if (this.filterOption === '1floor') {
-            processedItems = processedItems.filter((item) => item.houseFloors === 1)
-          } else if (this.filterOption === '2floor') {
-            processedItems = processedItems.filter((item) => item.houseFloors === 2)
-          } else if (this.filterOption === '3floor') {
-            processedItems = processedItems.filter((item) => item.houseFloors === 3)
-          }
+        if (this.filterOptions.length > 0) {
+          processedItems = processedItems.filter(item => this.filterOptions.includes(`${item.houseFloors}floor`));
         }
 
         // Sorting
-        if (this.sortOption) {
-          if (this.sortOption === 'name') {
+        this.sortOptions.forEach(sortOption => {
+          if (sortOption === 'name') {
             processedItems.sort((a, z) => a.houseName.localeCompare(z.houseName))
-          } else if (this.sortOption === 'price') {
-            processedItems.sort((low, high) => low.housePrice - high.housePrice)
-          } else if (this.sortOption === 'floors') {
+          } else if (sortOption === 'price') {
+            if (this.priceSortOrder === 'lowToHigh') {
+              processedItems.sort((low, high) => low.housePrice - high.housePrice)
+            } else {
+              processedItems.sort((low, high) => high.housePrice - low.housePrice)
+            }
+          } else if (sortOption === 'floors') {
             processedItems.sort((low, high) => low.houseFloors - high.houseFloors)
-          } else if (this.sortOption === 'bedrooms') {
+          } else if (sortOption === 'bedrooms') {
             processedItems.sort((low, high) => low.houseBedrooms - high.houseBedrooms)
-          } else if (this.sortOption === 'bathrooms') {
+          } else if (sortOption === 'bathrooms') {
             processedItems.sort((low, high) => low.houseBathrooms - high.houseBathrooms)
           }
-        }
+        });
 
-        return processedItems
+        return processedItems;
       }
     }
   }
@@ -65,123 +85,178 @@
 
 
 <template>
-  <!-- FILTER BUTTONS -->
-  <div class="filter-buttons">
-    <button
-      @click="handleFilterOption('1floor')"
-      :class="{ active: filterOption === '1floor' }"
-    >
-      1 Floor
-    </button>
-    <button
-      @click="handleFilterOption('2floor')"
-      :class="{ active: filterOption === '2floor' }"
-    >
-      2 Floor
-    </button>
-    <button
-      @click="handleFilterOption('3floor')"
-      :class="{ active: filterOption === '3floor' }"
-    >
-      3 Floor
-    </button>
-  </div>
+  <div class="container">
 
-  <!-- SORT BUTTONS -->
-  <div class="sort-buttons">
-    <button
-      @click="handleSortOption('name')"
-      :class="{ active: sortOption === 'name' }"
-    >
-      Sort by Name
-    </button>
-    <button
-      @click="handleSortOption('price')"
-      :class="{ active: sortOption === 'price' }"
-    >
-      Sort by Price
-    </button>
-    <button
-      @click="handleSortOption('floors')"
-      :class="{ active: sortOption === 'floors' }"
-    >
-      Sort by Floors
-    </button>
-    <button
-      @click="handleSortOption('bedrooms')"
-      :class="{ active: sortOption === 'bedrooms' }"
-    >
-      Sort by Bedrooms
-    </button>
-    <button
-      @click="handleSortOption('bathrooms')"
-      :class="{ active: sortOption === 'bathrooms' }"
-    >
-      Sort by Bathrooms
-    </button>
-  </div>
+    <div class="filer-container">
+      <div class="filter-section">
+      <h2>Filter</h2>
+      <div class="filter-options">
+        
+        <div class="filter-buttons">
+          <button @click="handleFilterOption('1floor')" :class="{ active: filterOptions.includes('1floor') }">
+            1 Floor
+          </button>
+          <button @click="handleFilterOption('2floor')" :class="{ active: filterOptions.includes('2floor') }">
+            2 Floors
+          </button>
+          <button @click="handleFilterOption('3floor')" :class="{ active: filterOptions.includes('3floor') }">
+            3 Floors
+          </button>
+        </div>
+      </div>
+    </div>
 
-  <div class="houseList" v-show="houseItemsProcessed">
-    <div
-      class="houseItem"
-      v-for="houseItem in houseItemsProcessed"
-      :key="houseItem.Id"
-    >
-      <p>Name: {{ houseItem.houseName }}</p>
-      <p>Price: {{ houseItem.housePrice }} $</p>
-      <p>Floors: {{ houseItem.houseFloors }}</p>
-      <p>Bedrooms: {{ houseItem.houseBedrooms }}</p>
-      <p>Bathrooms: {{ houseItem.houseBathrooms }}</p>
+    <div class="sort-section">
+      <h2>Sort</h2>
+      <div class="sort-buttons">
+        <button @click="handleSortOption('name')" :class="{ active: sortOptions.includes('name') }">
+          Sort by Name
+        </button>
+        <button @click="handleSortOption('price')" :class="{ active: sortOptions.includes('price') }">
+          Sort by Price ({{ sortOptions.includes('price') ? 'Low to High' : 'High to Low' }})
+        </button>
+        <button @click="handleSortOption('floors')" :class="{ active: sortOptions.includes('floors') }">
+          Sort by Floors ({{ sortOptions.includes('floors') ? 'Low to High' : 'High to Low' }})
+        </button>
+        <button @click="handleSortOption('bedrooms')" :class="{ active: sortOptions.includes('bedrooms') }">
+          Sort by Bedrooms ({{ sortOptions.includes('bedrooms') ? 'Low to High' : 'High to Low' }})
+        </button>
+        <button @click="handleSortOption('bathrooms')" :class="{ active: sortOptions.includes('bathrooms') }">
+          Sort by Bathrooms ({{ sortOptions.includes('bathrooms') ? 'Low to High' : 'High to Low' }})
+        </button>
+      </div>
+    </div>
+    </div>
+ 
+
+    <div class="houseList">
+      <div
+        class="houseItem"
+        v-for="houseItem in houseItemsProcessed"
+        :key="houseItem.Id"
+      >
+        <h3>{{ houseItem.houseName }}</h3>
+        <p>Price: {{ houseItem.housePrice }} $</p>
+        <p>Floors: {{ houseItem.houseFloors }}</p>
+        <p>Bedrooms: {{ houseItem.houseBedrooms }}</p>
+        <p>Bathrooms: {{ houseItem.houseBathrooms }}</p>
+      </div>
     </div>
   </div>
 </template>
 
+
+
 <style scoped>
-.filter-buttons {
+.container {
   display: flex;
-  margin-bottom: 10px;
+  flex-direction: column;
+  align-items: center;
+  font-family: Arial, sans-serif;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 20px;
 }
 
-.sort-buttons {
+.filer-container {
   display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+h2 {
+  margin-bottom: 10px;
+  text-transform: uppercase;
+  font-size: 22px;
+}
+
+h3 {
+  margin-bottom: 5px;
+  font-size: 18px;
+}
+
+.filter-section,
+.sort-section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   margin-bottom: 20px;
 }
 
+.filter-buttons,
+.sort-buttons {
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+  margin-bottom: 10px;
+}
+.filter-options {
+  display: flex;
+}
 .filter-buttons button,
 .sort-buttons button {
-  margin-right: 10px;
   padding: 8px 12px;
   font-size: 14px;
-  border: 1px solid #ccc;
-  background-color: transparent;
+  border-radius: 5px;
+  border: 2px solid rgba(0, 0, 0, 0.1);
+  background: rgba(255, 255, 255, 0.8);
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   cursor: pointer;
-}
-
-.filter-buttons button {
-  background-color: #2c3e50;
-  color: #fff;
-}
-
-.sort-buttons button {
-  background-color: #34495e;
-  color: #fff;
+  outline: none;
+  transition: all 0.2s;
 }
 
 .filter-buttons button.active,
 .sort-buttons button.active {
-  background-color: #e74c3c;
+  background: rgba(0, 150, 255, 0.8);
+  color: white;
+  border-color: rgba(0, 150, 255, 0.8);
 }
 
 .houseList {
   display: flex;
   flex-wrap: wrap;
+  justify-content: center;
   gap: 20px;
+  padding: 20px;
+  width: 100%;
+  background-color: rgba(255, 255, 255, 0.8);
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 }
 
 .houseItem {
-  padding: 15px;
-  border: 1px solid #ccc;
+  box-sizing: border-box;
+  flex-basis: calc(25% - 20px);
+  padding: 20px;
+  background-color: white;
   border-radius: 5px;
-  background-color: #f7f7f7;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  margin-bottom: 20px;
+}
+
+/* Media queries for responsiveness */
+@media screen and (max-width: 1200px) {
+  .houseItem {
+    flex-basis: calc(33.33% - 20px);
+  }
+}
+
+@media screen and (max-width: 992px) {
+  .houseItem {
+    flex-basis: calc(50% - 20px);
+  }
+}
+
+@media screen and (max-width: 576px) {
+  .houseItem {
+    flex-basis: calc(100% - 20px);
+  }
 }
 </style>
+
+
+
