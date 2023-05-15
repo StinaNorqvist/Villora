3c4a6b85a4c4621d4102c7d8bba0b478445c3107
lopesdaluz <tlopesdaluz@gmail.com>
2023-05-15 14:46:36 +0200

    removed some stuff

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -67,32 +67,6 @@
       <img src="../../assets/images/book.jpeg" alt="book image" id="bookImg" />
     </div>
   </div>
-  <h1>h1 text</h1>
-  <h2>h2 text</h2>
-  <p>p text vanlig</p>
-  <p class="cursive">class="cursive"</p>
-  <p class="light" style="background-color: black">
-    ändra text till vit färg skriv class="light"
-  </p>
-  <p class="lightbrown light">
-    för att sätta bakgrundsfärg kan ni skriva class="lightbrown"
-  </p>
-  <p class="darkbrown light">eller class="darkbrown"</p>
-  <div class="glassEffect">
-    <p>
-      finns class="glassEffect" som är för bakgrunden på login, create account
-      och chatten
-    </p>
-  </div>
-  <div>
-    <p>samling av bootstrap iconer:</p>
-    <i class="bi bi-heart" />
-    <i class="bi bi-heart-fill" />
-    <i class="bi bi-chat" />
-    <i class="bi bi-send" />
-    <i class="bi bi-x" />
-    <i class="bi bi-arrow-left" />
-  </div>
 </template>
 <style scoped>
   /* section 1 */
